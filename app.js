1cb611e3cc91cc1a47080531154de38bf5eb1d42
Sean Godard <godard.sean@gmail.com>
2014-03-25 13:41:55 -0400

    Added a default route.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', require('./routes/index'));
+app.get('*', require('./routes/default'));
 
 app.listen(8080);
 console.log('Server is up.');
