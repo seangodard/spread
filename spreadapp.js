7d28ecb741bda1a6e66f59dd44660ef82f263155
Sean Godard <godard.sean@gmail.com>
2014-04-08 14:14:22 -0400

    Added basic redirects to spreadapp.js and made a skeleton error page.

--- spreadapp.js
+++ spreadapp.js
@@ -1,8 +1,28 @@
+// Redirecting to various routes
 var express = require('express');
+
+// Create a server
 var app = express();
 
-app.get('/', require('./routes/index'));
-app.get('*', require('./routes/default'));
+var db = mongojs('visitapp',['visitors']);
+
+// Configure the server
+app.set('view engine', 'ejs');
+app.set('views', '/spread/views');
+app.use(express.urlencoded({limit:'1kb'})); // default for every route/ prevent form inputs over 1 kb
+app.use(express.static('/spread/statics'));
+
+app.get('/', require('./routes/home'));
+app.get('/register', require('./routes/registration'));
+app.get('/searchresults', require('./routes/search'));
+
+app.get('/profile', require('./routes/profile'));
+app.get('/history', require('./routes/history'));
+app.get('/manageaccount', require('./routes/manageaccount'));
+app.get('/inbox', require('./routes/inbox'));
+app.get('/myspread', require('./routes/myspread'));
+
+app.get('*', require('./routes/error'));
 
 app.listen(8080);
 console.log('Server is up.');
