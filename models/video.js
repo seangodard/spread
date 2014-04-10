7f25084784ced364192a28f2b4fb3edee60b9c0f
Jose Alejandro <jalej10@gmail.com>
2014-04-10 12:55:15 -0400

    Moved post new video to video.js

--- models/video.js
+++ models/video.js
@@ -3,11 +3,12 @@ var mongojs = require('mongojs');
 var bcrypt = require('bcrypt');
 
 // Connect to the database spreadapp, collection: users
-var db = mongojs('spreadapp', ['users']);
+var db = mongojs('spreadapp', ['videos']);
 
 // Add User: username, password, firstname, lastname, email
 // Optional: promoted video url, pic, bio
-module.exports.adduser = function(username, password, first_name, last_name, email, promoted_video_url, pic, bio, callback) {    
+module.exports.post_new_video = function(username, password, first_name,
+        last_name, email, promoted_video_url, pic, bio, callback) {    
     bcrypt.hash(password, 10, function(error,hash) {
         if (error) throw error;
         
@@ -15,7 +16,9 @@ module.exports.adduser = function(username, password, first_name, last_name, ema
         db.users.findAndModify({
             query: {username:username},/*search criteria*/
             /*field to change*/
-            update: {$setOnInsert:{username:username, password:hash,first_name:first_name,last_name:last_name, email:email, promoted_video_url:promoted_video_url, pic:pic, bio:bio}},
+            update: {$setOnInsert:{username:username, password:hash,
+            first_name:first_name,last_name:last_name, email:email,
+            promoted_video_url:promoted_video_url, pic:pic, bio:bio}},
             /*says to return modified version*/
             new: true,
             /*create a new document if there wasn't one*/
@@ -36,44 +39,3 @@ module.exports.adduser = function(username, password, first_name, last_name, ema
         });    
     });
 };
-
-// Delete all users in collection
-module.exports.deleteAll = function(callback) {
-    db.users.remove({}, function(error) {
-        if (error) throw error;
-        callback();
-    });
-};
-
-// Close the connection
-module.exports.close = function(callback) {
-    db.close(function(error) {
-        if (error) throw error;
-        callback();
-    });
-}
-
-// Verify credentials for login
-module.exports.login = function(username, password, callback) {
-    
-    db.users.findOne({username:username}, function(error, user) {
-        if (error) throw error;
-        
-        if (!user) {
-            callback(false);
-        }
-        
-        else {
-            bcrypt.compare(password, user.password, function(error, success) {
-                if (error) throw error;
-                
-                callback(success);
-            })
-        }
-    });
-};
-
-
-
-
-
