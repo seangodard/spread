bae862aa4ad2d12917901b072fd47baccd12644c
Sean Godard <godard.sean@gmail.com>
2014-04-15 12:40:47 -0400

    Updated posts functions and test code.

--- models/posts.js
+++ models/posts.js
@@ -1,39 +1,40 @@
 // Database functions
 var mongojs = require('mongojs');
 
-// Connect to the database spreadapp, collection: users
+// Connect to the database spreadapp, collection: posts
 var db = mongojs('spreadapp', ['posts']);
 
 // Add a new post
 module.exports.add_post = function(username, timestamp, subject, body, callback) {
-    var success = true;
-    db.posts.insert({timestamp:timestamp, subject:subject, body:body}, function(error) {
+    db.posts.insert({username:username, timestamp:timestamp, subject:subject, body:body}, function(error) {
         if (error) throw error;
-        success = false;
     });
-    callback(success);
+    callback();
+};
+
+// Retrieve posts
+module.exports.retrieve_posts = function (username, timestamp, callback) {
+    db.posts.find({username:username, timestamp:timestamp}, function(error,posts) {
+        if (error) throw error;
+        callback(posts);
+    });
+};
+
+// Edit a post
+module.exports.edit_post = function(username, timestamp, newsubject, newbody, callback) {
+    db.posts.update({username:username,timestamp:timestamp},{$set: {subject:newsubject, body:newbody}}, function(error) {
+        if (error) throw error;
+    });
+    callback();
 };
 
 // Delete a post
 module.exports.delete_post = function(username, timestamp, subject, body, callback) {
-    var success = true;
     db.posts.remove({username:username, timestamp:timestamp, subject:subject, body:body},{justOne:true}, function(error) {
         if (error) throw error;
-        success = false;
     });
-    callback(success);
+    callback();
 };
-
-// Edit a post
-module.exports.edit_post = function() {
-    
-};
-
-// Retrieve posts
-module.exports.retrieve_posts = function () {
-    
-};
-
 
 // Delete all posts in collection
 module.exports.deleteAll = function(callback) {
--- tests/unit/posts.js
+++ tests/unit/posts.js
@@ -10,18 +10,44 @@ exports['setup'] = function(test) {
 
 // Test add a post
 exports['post'] = function(test) {
+    posts.add_post('Alice', 'timestamp','Subject: Party', 'Body: At my house', function() {
+        test.done();
+    });
+};
+
+// Test retrieve a post successful
+exports['retrieve post successful'] = function(test) {
     test.expect(1);
-    posts.add_post('username', 'timestamp','subject', 'body', function(success) {
-        test.ok(success);
+    var success = false;
+    posts.retrieve_posts('Alice', 'timestamp' , function(posts) {
+        posts.forEach(function(post) {
+            if (post.username === 'Alice' && post.timestamp === 'timestamp') {
+                success = true;
+            }
+        });
+        test.ok(posts.length === 1 && success);
+        test.done();
+    });
+};
+
+// Test retrieve a post unsuccessful
+exports['retrieve post unsuccessful'] = function(test) {
+    test.expect(1);
+    var success = false;
+    posts.retrieve_posts('Bob', 'timestamp' , function(posts) {
+        posts.forEach(function(post) {
+            if (post.username === 'Bob' && post.timestamp === 'timestamp') {
+                success = true;
+            }
+        });
+        test.ok(!(posts.length === 1 && success));
         test.done();
     });
 };
 
 // Test delete a post
 exports['delete post'] = function(test) {
-    test.expect(1);
-    posts.delete_post('username', 'timestamp', 'subject', 'body', function(success) {
-        test.ok(success);
+    posts.delete_post('Alice', 'timestamp', 'subject', 'body', function() {
         test.done();
     });
 };
